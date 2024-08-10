b3825b7d0b6e8eb33e817b8f275aa8a4f323ea5f
bondyaroslav <bondy2526@gmail.com>
2024-08-10 18:09:03 +0300

    Add button in HotelItem

--- src/components/hotels/HotelItem.tsx
+++ src/components/hotels/HotelItem.tsx
@@ -1,22 +1,31 @@
 import { IHotel } from '@/types/IHotel'
 import Image from 'next/image'
 import style from './HotelItem.module.css'
-import { Box } from '@mui/material'
+import { Box, Button } from '@mui/material'
+import heart from '../../assets/hotels/heart.svg'
 
-const HotelItem = ({name, location, image, pricePerNight, category, amenities, about}: IHotel) => {
+const HotelItem = ({name, location, image, pricePerNight, amenities, about}: IHotel) => {
     return (
         <article className={style.item}>
-            <Image
-                className={style.image}
-                src={image}
-                width={250}
-                height={150}
-                alt={'img'}
-            />
+            <Box className={style.imageWrapper}>
+                <Image
+                    className={style.image}
+                    src={image}
+                    width={250}
+                    height={150}
+                    alt={'img'}
+                />
+                <Button className={style.button}>
+                    <Image src={heart} alt={'img'} style={{margin: 5}}/>
+                    LÄGG TILL
+                </Button>
+            </Box>
             <Box className={style.description}>
                 <Box className={style.amenities}>
                     {amenities.map((amenity, index) => (
-                        <Box className={style.amenity} key={index}>{amenity}</Box>
+                        <Box className={style.amenity} key={index}>
+                            {amenity}
+                        </Box>
                     ))}
                 </Box>
                 <h3 className={style.name}>{name.toUpperCase()}</h3>
